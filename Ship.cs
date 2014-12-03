9c6663e9cba567e9d1e6a29ffdea0d718e9a7c2d
Patrick Barnum <admin@patrickbarnum.com>
2014-12-03 09:51:23 -0800

    Adds health and player chosen texture

--- Ship.cs
+++ Ship.cs
@@ -3,38 +3,31 @@ using System.Collections;
 
 public class Ship : MonoBehaviour
 {
-	//private mesh
-	//private texture
-	//private indicator
+    public Material texture;
 
-    public int playerHealth = 5;
+    public int playerHealth = 3;
     public float maxWarp = 2.05f;
     public float baseSpeed = 10f;
     public float baseStrafe = 20f;
     public float maxSpeed = 30f;
 
     public Animator animator;
-	
+
 	// Use this for initialization
 	void Start ()
     {
         // Start ship in grounded animation
         animator = this.GetComponent<Animator>();
         animator.Play("grounded");
-	
-        //switch()
-        //{
-        //case 0:
-        //    break;
-        //case 1:
-        //    break;
-        //case 2:
-        //    break;
-        //case 3:
-        //    break;
-        //default:
-        //    break;
-        //}
+
+        if(PlayerPrefs.HasKey("material"))
+        {
+            this.transform.FindChild("LiftOff:Ship").renderer.material = Resources.Load<Material>("ship/" + PlayerPrefs.GetString("material"));
+        }
+        else
+        {
+            this.transform.FindChild("LiftOff:Ship").renderer.material = Resources.Load<Material>("ship/ShipDiffuse_Red");
+        }
 	}
 	
 	// Update is called once per frame
